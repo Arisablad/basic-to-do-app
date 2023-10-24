d986ab530735babd30c3f9debb0ef7ebc4fa05cb
udaceue <rjemik9@gmail.com>
2023-10-24 10:42:43 +0200

    Update TodosModels.ts

--- src/app/models/TodosModels.ts
+++ src/app/models/TodosModels.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const TodoSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true, maxLength: 90 },
-    done: { type: String },
+    done: { type: Boolean, default: false },
 })
 
 export default mongoose.models.Todo || mongoose.model("Todo", TodoSchema)
